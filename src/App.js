af8afe5a0489257cb68b103e53dc708f0eb6f0a3
Diyan Georgiev <dgeorgiev_job@yahoo.com>
2023-03-26 19:38:33 +0300

    first message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import {
+  BrowserRouter as Router, Routes, Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>House market </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,8 @@
+
+export default function Signin() {
+    return (
+        <div>
+            <h1>Sign in</h1>
+        </div>
+    )
+}
