6808e5b29571e9b3b4d0c18f9670ee84eefba4c4
dgeorgiev <dgeorgiev_job@yahoo.com>
2023-04-01 23:26:11 +0200

    Add logic for Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,15 @@
 
+import { getAuth } from "firebase/auth"
+import { useEffect } from "react"
 
 function Profile() {
+
+    const auth = getAuth()
+    useEffect(() => {
+        console.log(auth);
+
+    }, [])
+
     return (
         <div>
             <h1>Profile</h1>
